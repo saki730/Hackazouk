2dfe897aec276e00fb52a165c0bfd73781e67956
saki <aoyama.s.730@gmail.com>
2025-02-02 13:24:20 +0900

    20250202_deploy

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -3,12 +3,21 @@
 import React from 'react'
 import styles from './style.module.scss'
 
-const Post = () => {
+type PostProps = {
+    id: number;
+    name: string;
+    date: string;
+    content: string;
+  }
+
+
+const Post: React.FC<PostProps> = ({ id, name, date, content }) => {
   return (
     <div className={styles.post}>
-      <p>名前が入る予定</p>
-      <p>日付が入る予定</p>
-      <p>登録されたデータを表示する予定</p>
+      <p>{id}</p>
+      <p>{name}</p>
+      <p>{date}</p>
+      <p>{content}</p>
       <p className={styles.link}>リンクが入ります</p>
     </div>
   )
